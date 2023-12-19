1a93cb22e7166f335d4c2ba961c8ae2f1c65b90e
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2023-12-19 13:44:42 +0100

    Update frontend to accomodate multiple chatbots

--- frontend/app.py
+++ frontend/app.py
@@ -6,20 +6,28 @@ app = Flask(__name__)
 BACKEND_SERVER_URL = "http://127.0.0.1:5000"
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/<tool>', methods=['GET', 'POST'])
+def chatbot(tool):
+    if tool not in ['archicad', 'rfem']:
+        return render_template("404.html")
+
     if request.method == 'POST':
         user_input = request.form['user_input']
 
-        backend_payload = {'request': user_input}
+        backend_payload = {'request': user_input, 'tool': tool}
         backend_response = requests.post(BACKEND_SERVER_URL, json=backend_payload)
 
         if backend_response.status_code == 200:
             answer = backend_response.text
-            return render_template("index.html", answer=answer)
+            return render_template("chatbot.html", answer=answer, tool=tool)
         else:
             return 'Error communicating with the backend server', 500
 
+    return render_template("chatbot.html", tool=tool)
+
+
+@app.route('/', methods=['GET'])
+def index():
     return render_template("index.html")
 
 
