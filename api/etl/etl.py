dfbed37a48365c1bb972f490918cead78346e2bf
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2023-12-19 17:03:10 +0100

    Extend etl pipeline to be usable for multiple input sources
    The document to be processed needs to be in the docs folder.
    Two positional arguments were added to etl.py
    The first determines the filename of the file to be processed
    The second should be the tool name and determines the namespace
    for the vector database

--- api/etl/etl.py
+++ api/etl/etl.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import argparse
 
 from pdf_reader import extract_text_from_pdf
 from shared.embedder import Embedder
@@ -6,16 +7,22 @@ from shared import db
 
 
 docs_path = Path.cwd().parent.parent / 'docs'
-archicad_manual_path = docs_path / 'Archicad.pdf'
+parser = argparse.ArgumentParser()
 
 embedder = Embedder()
 
 if __name__ == '__main__':
+    parser.add_argument('filename')
+    parser.add_argument('tool')
+    args = parser.parse_args()
+
+    manual_path = docs_path / args.filename
+
     print("Extracting content from pdf ...")
-    pdf_content = extract_text_from_pdf(archicad_manual_path)
+    pdf_content = extract_text_from_pdf(manual_path)
     print("Retrieve embeddings for content ...")
     embeddings = embedder.embed(pdf_content)
     print("Index documents ...")
     docs = [{"id": str(i), "values": embeddings[i].tolist(), "metadata": {"text": pdf_content[i]}}
             for i in range(len(pdf_content))]
-    db.index_docs(docs)
+    db.index_docs(docs, namespace=args.tool)
--- api/shared/db.py
+++ api/shared/db.py
@@ -7,11 +7,11 @@ max_docs_indexed_at_once = 1000
 
 def get_index():
     pinecone.init(api_key=os.environ['PINECONE_API_KEY'], environment=os.environ['PINECONE_ENV'])
-    return pinecone.Index("archicad")
+    return pinecone.Index("doc-rag")
 
 
-def index_docs(docs):
+def index_docs(docs, namespace):
     index = get_index()
 
     for i in tqdm(range(len(docs) // max_docs_indexed_at_once + 1)):
-        index.upsert(vectors=docs[i*max_docs_indexed_at_once:(i+1)*max_docs_indexed_at_once], namespace="archicad")
+        index.upsert(vectors=docs[i*max_docs_indexed_at_once:(i+1)*max_docs_indexed_at_once], namespace=namespace)
