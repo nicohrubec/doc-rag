114783d95d8fd7aad1b6bb4cfccf06f75b2eb4c2
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2023-12-20 22:12:27 +0100

    Change backend port to 8000

--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-BACKEND_SERVER_URL = "http://127.0.0.1:5000"
+BACKEND_SERVER_URL = "http://127.0.0.1:8000"
 
 
 @app.route('/<tool>', methods=['GET', 'POST'])
