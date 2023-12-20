f28b51bb3810ea80460ecc558ef6f54f1628a145
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2023-12-20 11:32:25 +0100

    Split documents into 1024 chunks

--- api/etl/pdf_reader.py
+++ api/etl/pdf_reader.py
@@ -1,14 +1,20 @@
 import fitz
 from tqdm import tqdm
+from llama_index.node_parser import SentenceSplitter
 
 
 def extract_text_from_pdf(pdf_path):
     pdf_document = fitz.open(pdf_path)
     content = []
 
+    text_parser = SentenceSplitter(
+        chunk_size=1024
+    )
+
     for page_number in tqdm(range(pdf_document.page_count)):
-        page = pdf_document[page_number]
-        content.append(page.get_text())
+        page_text = pdf_document[page_number].get_text()
+        cur_text_chunks = text_parser.split_text(page_text)
+        content.extend(cur_text_chunks)
 
     pdf_document.close()
 
